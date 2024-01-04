893fd3842b4ba309136c382a49a5a4173dcc4324
= <=>
2024-01-04 08:32:20 -0500

    moved all static variables into main script. next step to make them environment variables

--- JiraUtils.py
+++ JiraUtils.py
@@ -1,33 +1,31 @@
 from jira import JIRA
-import os
 
-jira_server = "https://jirab.statcan.ca"
-jira_auth_token = os.environ.get("JIRA_TOKEN")
-jira_project = "DASBOP"
-jira_issue_type = "Epic"
-jira_assignee = "luodan"
-jira_watchers = ["zimmshe", "bonedan", "coutann"]
+class JiraUtils:
 
-jira = JIRA(server=jira_server,
-            token_auth=jira_auth_token)
+    # parameterized constructor
+    def __init__(self, jira_server, jira_auth_token, jira_project, jira_issue_type, jira_assignee, jira_watchers):
+        
+        self.jira = JIRA(server=jira_server, token_auth=jira_auth_token)
+        self.jira_project = jira_project
+        self.jira_issue_type = jira_issue_type
+        self.jira_assignee = jira_assignee
+        self.jira_watchers = jira_watchers
 
-def create_jira_issue_from_form_data(issue_summary, issue_desc):
-    issue_dict = {
-        'project': {'key': jira_project},
-        'summary': issue_summary,
-        # epic name field
-        'customfield_10704': issue_summary,
-        'description': issue_desc,
-        'issuetype': {'name': jira_issue_type},
-        'assignee': {'name': jira_assignee}
-    }
+    def create_jira_issue_from_form_data(self, issue_summary, issue_desc):
+        
+        issue_dict = {
+            'project': {'key': self.jira_project},
+            'summary': issue_summary,
+            # epic name field
+            'customfield_10704': issue_summary,
+            'description': issue_desc,
+            'issuetype': {'name': self.jira_issue_type},
+            'assignee': {'name': self.jira_assignee}
+        }
 
-    new_issue = jira.create_issue(fields=issue_dict)
-    # this can't be done as part of issue creation, unfortunately
-    for watcher in jira_watchers:
-        jira.add_watcher(new_issue, watcher)
+        new_issue = self.jira.create_issue(fields=issue_dict)
+        # this can't be done as part of issue creation, unfortunately
+        for watcher in self.jira_watchers:
+            self.jira.add_watcher(new_issue, watcher)
 
-    return new_issue
-
-def get_authenticated_user():
-    return f"Authenticated user: {jira.myself()['name']}"
+        return new_issue
--- Sp2jira.py
+++ Sp2jira.py
@@ -1,6 +1,36 @@
+import os
 import pandas as pd
-import SharepointUtils
-import JiraUtils
+from JiraUtils import JiraUtils
+from SharepointUtils import SharepointUtils
+
+# initialize JIRA
+jira_server = "https://jirab.statcan.ca"
+jira_auth_token = os.environ.get("JIRA_TOKEN")
+jira_project = "DASBOP"
+jira_issue_type = "Epic"
+jira_assignee = "luodan"
+# https://stackoverflow.com/questions/31352317/how-to-pass-a-list-as-an-environment-variable
+jira_watchers = ["zimmshe", "bonedan", "coutann"]
+
+jira = JiraUtils(jira_server, jira_auth_token, jira_project, jira_issue_type, jira_assignee, jira_watchers)
+
+# sharepoint connection
+client_id = os.environ.get("SHAREPOINT_CLIENT_ID") 
+client_secret = os.environ.get("SHAREPOINT_CLIENT_SECRET")
+site_url = "https://054gc.sharepoint.com/sites/DAaaSD-AllStaff-DADS-Touslesemployes"
+
+#form data
+# xslx can be found at https://054gc.sharepoint.com/:x:/r/sites/DAaaSD-AllStaff-DADS-Touslesemployes/_layouts/15/Doc.aspx?sourcedoc=%7B1C1CBFA7-E2E3-4BCF-8030-94C3A4384715%7D&file=Data%20Analytics%20Services%20(DAS)%20-%20Get%20started.xlsx&action=default&mobileredirect=true&cid=2ac2f7e2-eea5-471f-abe7-3614f5cb5fcd 
+file_url = "/sites/DAaaSD-AllStaff-DADS-Touslesemployes/Shared%20Documents/CSU%20-%20UCS/DAaaS%20Intake%20Form/Data%20Analytics%20Services%20(DAS)%20-%20Get%20started%201.xlsx"
+sheet_name = "Form1"
+
+#processed id list data
+# list can be found at https://054gc.sharepoint.com/sites/DAaaSD-AllStaff-DADS-Touslesemployes/Lists/Intake_form_last_id/AllItems.aspx
+list_title = "Intake_form_processed_ids" #name of the list in sharepoint
+list_column = "Title" #the list column containing ID data
+list_max_return = 5000 #if we ever get more applications than this we'll have to adjust it
+
+sputils = SharepointUtils(client_id, client_secret, site_url, file_url, sheet_name, list_title, list_column, list_max_return)
 
 
 # the column where the form stores the ID value. It seems to get populated only when data is submitted via the form.
@@ -11,7 +41,7 @@ jira_issue_summary = "DAS Intake Form submission by {0} {1}"
 jira_desc_no_response = "No Response"
 
 # get the form data from sharepoint
-df = SharepointUtils.get_intake_form_data_as_dataframe()
+df = sputils.get_intake_form_data_as_dataframe()
 
 ## go through each row and create a JIRA issue, saving processed IDs to the sharepoint list so we don't create them again later
 issue_count = 0
@@ -33,11 +63,11 @@ for index, row in df.iterrows():
     #print(issue_desc) #left for debug
 
     try:
-        new_issue = JiraUtils.create_jira_issue_from_form_data(issue_summary, issue_desc)
+        new_issue = jira.create_jira_issue_from_form_data(jira_project, jira_issue_type, jira_assignee, issue_summary, issue_desc, jira_watchers)
     except:
         print(f"Error creating JIRA issue from ID {current_id}")
     else:
-        SharepointUtils.add_processed_id_to_list(current_id)
+        sputils.add_processed_id_to_list(current_id)
         issue_count += 1
         print(new_issue)
 
